c23975bd010d07fd90643c6e7fca989a84d65cc2
zz <zz@qq.com>
2021-06-14 18:24:55 +0800

    Xinf分支的第二次提交。

--- src/main/java/study/boot/demo/entity/Zzz.java
+++ src/main/java/study/boot/demo/entity/Zzz.java
@@ -7,5 +7,6 @@ public class Zzz {
         System.out.println("hello,Xinf!");
         System.out.println("hello,world!");
         System.out.println("hello,Zzz!");
+        System.out.println("hello,Zzz!");
     }
 }
