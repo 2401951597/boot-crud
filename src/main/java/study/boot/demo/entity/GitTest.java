e8eabb28b90f7888757b2998ad5418496098c3f5
zz <zz@qq.com>
2021-06-14 19:30:24 +0800

    push

--- src/main/java/study/boot/demo/entity/GitTest.java
+++ src/main/java/study/boot/demo/entity/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,Zzz!");
         System.out.println("hello,Zzz!");
         System.out.println("zzzzzz");
+        System.out.println("push");
     }
 }
