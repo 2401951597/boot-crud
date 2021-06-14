c44fcf70432532d8cd9822dc1d30ad24535ee88a
zz <zz@qq.com>
2021-06-14 18:47:43 +0800

    Xinf的第五次提交。

---
+++ src/main/java/study/boot/demo/entity/GitTest.java
@@ -0,0 +1,13 @@
+package study.boot.demo.entity;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello,git!");
+        System.out.println("hello,Xinf!");
+        System.out.println("hello,world!");
+        System.out.println("hello,Zzz!");
+        System.out.println("hello,Zzz!");
+        System.out.println("zzzzzz");
+    }
+}
