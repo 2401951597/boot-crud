4605bc731226d58fe7521040462b3beedbf4463e
zz <zz@qq.com>
2021-06-14 18:20:20 +0800

    Xinf分支的第一次提交。

--- src/main/java/study/boot/demo/entity/Zzz.java
+++ src/main/java/study/boot/demo/entity/Zzz.java
@@ -6,5 +6,6 @@ public class Zzz {
         System.out.println("hello,git!");
         System.out.println("hello,Xinf!");
         System.out.println("hello,world!");
+        System.out.println("hello,Zzz!");
     }
 }
